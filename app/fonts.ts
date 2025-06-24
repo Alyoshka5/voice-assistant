bf3adf7810007c2d9fc910f2aac483baf3186ae3
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-23 21:08:43 -0700

    style: convert all indents from 2 to 4 spaces

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,8 +1,8 @@
 import { Spectral } from 'next/font/google';
 
 const primaryFont = Spectral({
-  subsets: ['latin'],
-  weight: ['400', '700'],
+    subsets: ['latin'],
+    weight: ['400', '700'],
 });
 
 export default primaryFont;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import primaryFont from "./fonts";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to APEX</h1>
-      <div>
-        <p>Get started:</p>
-        <input type="button" className={`${primaryFont.className}`}value="Sign in with Google" />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to APEX</h1>
+            <div>
+                <p>Get started:</p>
+                <input type="button" className={`${primaryFont.className}`}value="Sign in with Google" />
+            </div>
+        </div>
+    );
 }
