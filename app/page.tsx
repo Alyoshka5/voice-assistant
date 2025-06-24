8f3b760f5ba0939b23f4d55b58d57e8aa0b923b3
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-23 17:41:51 -0700

    feat: create basic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div>
-      
+      <h1>Welcome to APEX</h1>
+      <div>
+        <p>Get started:</p>
+        <input type="button" value="Sign in with Google" />
+      </div>
     </div>
   );
 }
