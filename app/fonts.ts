ab3a974d0c96e023dc0d5c43855a0f6d1a31fef3
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-23 17:49:36 -0700

    feat(font): change primary font

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Spectral } from 'next/font/google';
+
+const primaryFont = Spectral({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export default primaryFont;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import primaryFont from "./fonts";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to APEX</h1>
       <div>
         <p>Get started:</p>
-        <input type="button" value="Sign in with Google" />
+        <input type="button" className={`${primaryFont.className}`}value="Sign in with Google" />
       </div>
     </div>
   );
