907dd6d7c783fe448b8083d9d14e549b2600d376
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-25 14:38:01 -0700

    feat(db): create user on signin if email not in db

--- auth.ts
+++ auth.ts
@@ -1,6 +1,27 @@
 import NextAuth from "next-auth"
 import Google from 'next-auth/providers/google'
+import prisma from '@/app/lib/db'
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Google],
+    callbacks: {
+        async signIn({ profile }) {
+            if (!profile) return false;
+
+            const existingUser = await prisma.user.findUnique({
+                where: { email: profile.email ?? undefined}
+            })
+
+            if (!existingUser && profile.email) {
+                await prisma.user.create({
+                    data: {
+                      name: profile.name,
+                      email: profile.email || '',
+                    },
+                })
+            }
+
+            return true;
+        }
+    }
 })
