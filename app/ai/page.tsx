739958f17930fe81e3c0416fd0c45ffe48a668ad
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-25 21:25:32 -0700

    feat(middleware): redirect user based on session and path

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,12 +1,8 @@
 import { auth } from "@/auth"
-import { redirect } from "next/navigation";
 
 export default async function Assistant() {
     const session = await auth();
-    if (!session) {
-        redirect("/");
-    }
-    const user = session.user;
+    const user = session?.user;
 
     return <h1>Welcome {user!.name} to APEX</h1>
 }
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,27 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    const session = await auth();
+
+    const { pathname } = request.nextUrl;
+
+    if (pathname === '/' && session?.user) {
+        return NextResponse.redirect(new URL('/ai', request.url));
+    }
+
+    const isProtectedRoute = pathname !== '/' && !pathname.startsWith('/api');
+    if (isProtectedRoute && !session?.user) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+      "/",
+      "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    ],
+  };
