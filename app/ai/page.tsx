11973e7d9aa355e3bb5a4ef87fa41bc58dec4834
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-25 21:33:37 -0700

    feat: add sign out option to /ai page

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,8 +1,17 @@
 import { auth } from "@/auth"
+import signOut from "@/app/lib/signout";
 
 export default async function Assistant() {
     const session = await auth();
     const user = session?.user;
 
-    return <h1>Welcome {user!.name} to APEX</h1>
+    return (
+        <div>
+            <h1>Welcome {user!.name} to APEX</h1>
+            <form action={signOut}>
+                <button type="submit">Sign Out</button>
+            </form>
+        </div>
+    )
+    
 }
