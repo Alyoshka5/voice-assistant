45a1c8b197f47d663edec8dc09a0593905258f9a
Alyoshka <alyoshkamyshko@gmail.com>
2025-06-30 16:05:21 -0700

    feat: add system message that will categorise user's request

--- app/actions/getOpenAIResponse.ts
+++ app/actions/getOpenAIResponse.ts
@@ -2,11 +2,28 @@
 
 import openAIClient from "@/app/lib/openai";
 
+const systemMessage = `
+You are a voice assistant. Classify the user's request into one of these categories:
+
+- weather
+- other
+
+Respond with a just the category name, without any additional text or explanation.
+
+If you're unsure, return "other". Do not extract details or guess.
+
+`;
+
 export default async function getOpenAIResponse(prompt: string) {
-    const response = await openAIClient.responses.create({
+    const promptCategoryResponse = await openAIClient.responses.create({
         model: 'gpt-4.1-nano',
-        input: prompt,
+        input: [
+            {role: 'system', content: systemMessage},
+            {role: 'user', content: prompt},
+        ],
     });
 
-    return response.output_text;
+    const promptCategory = promptCategoryResponse.output_text;
+
+    return promptCategory;
 }
