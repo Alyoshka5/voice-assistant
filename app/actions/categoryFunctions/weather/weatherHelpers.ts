5a10a70c1154514e20f2e4096266aa4de1a8ae6d
Alyoshka <alyoshkamyshko@gmail.com>
2025-07-01 11:50:17 -0700

    feat: add helper function to find coordinates for a given location

---
+++ app/actions/categoryFunctions/weather/weatherHelpers.ts
@@ -0,0 +1,25 @@
+
+async function getCoordinates(location:any) {
+    const nominatimUrl = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(location)}&format=json&limit=1`;
+    
+    try {
+        const response = await fetch(nominatimUrl, {headers: {
+            'User-Agent': 'ApexAssistant/1.0 (alyoshkamyshko@gmail.com)',
+            'Referer': 'https://apexassistant.vercel.app'
+        }});
+        const data = await response.json();
+    
+        if (data.length > 0) {
+            return {
+                latitude: data[0].lat,
+                longitude: data[0].lon
+            };
+        } else {
+            return {}
+        }
+    } catch (error) {
+        return {}
+    }
+}
+
+export { getCoordinates }
