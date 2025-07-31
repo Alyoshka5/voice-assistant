fa8b38f878175b6c52ad3e3dcc5209de3c57808e
Alyoshka <alyoshkamyshko@gmail.com>
2025-07-30 20:02:36 -0700

    feat: add jwt and session callbacks to get access token for Google APIs

--- auth.ts
+++ auth.ts
@@ -3,7 +3,16 @@ import Google from 'next-auth/providers/google'
 import prisma from '@/app/lib/db'
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+    providers: [Google({
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        authorization: {
+            params: {
+                scope: 'openid email profile https://www.googleapis.com/auth/youtube.force-ssl',
+                access_type: 'offline',
+            }
+        }
+    })],
     callbacks: {
         async signIn({ profile }) {
             if (!profile) return false;
@@ -22,6 +31,18 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             }
 
             return true;
-        }
+        },
+        async jwt({ token, account }) {
+            if (account) {
+              token.accessToken = account.access_token;
+              token.refreshToken = account.refresh_token;
+            }
+            return token;
+          },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken as string | undefined;
+            session.refreshToken = token.refreshToken as string | undefined;
+            return session;
+        },
     }
 })
---
+++ next-auth.d.ts
@@ -0,0 +1,20 @@
+import NextAuth, { DefaultSession } from 'next-auth';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+
+  interface User {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    accessToken?: string;
+    refreshToken?: string;
+  }
+}
