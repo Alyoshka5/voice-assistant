c0d1f00f21032f15eb589c40dc3e62ecca022cc7
Alyoshka <alyoshkamyshko@gmail.com>
2025-07-31 19:47:27 -0700

    feat: add initial setup that allows user to communicate with assistant

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,13 +1,60 @@
-import { auth } from "@/auth"
-import signOut from "@/app/actions/signout";
+'use client'
 
-export default async function Assistant() {
-    const session = await auth();
-    const user = session?.user;
+import signOut from "@/app/actions/signout";
+import useOpenAI from "@/app/hooks/useOpenAI";
+import { useEffect, useState } from "react";
+
+export default function Assistant() {
+    const [assistantResponseText, setAssistantResponseText] = useState<string>('');
+    const [currentCoords, setCurrentCoords] = useState<{ latitude: number, longitude: number } | null>(null);
+
+    const { getResponse } = useOpenAI();
+
+    const getAssistantResponse = async (formData: FormData) => {
+        const date = new Date();
+        const localDate = date.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+
+        const query: string = formData.get('textQuery') as string;
+        if (!query || query.trim() === '')
+            return;
+
+        const output = await getResponse(query, {
+            coordinates: currentCoords, 
+            date: localDate
+        });
+
+        if (output !== null && output !== '') {
+            setAssistantResponseText(output.outputText);
+        
+        }
+    }
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setCurrentCoords({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        });
+    }, []);
 
     return (
         <div>
-            <h1>Welcome {user!.name} to APEX</h1>
+            <form action={getAssistantResponse}>
+                <input
+                    type="text"
+                    name="textQuery"
+                    placeholder="Type to Apex..."
+                />
+                <button type="submit">Send</button>
+            </form>
+            <p>{assistantResponseText}</p>
+
             <form action={signOut}>
                 <button type="submit">Sign Out</button>
             </form>
