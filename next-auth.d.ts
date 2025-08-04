096fd0517c8c72f62308edb723361945eeb1c163
Alyoshka <alyoshkamyshko@gmail.com>
2025-08-04 16:18:59 -0700

    style: changing indent spacing from 2 to 4

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,20 +1,20 @@
 import NextAuth, { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
-  interface Session extends DefaultSession {
-    accessToken?: string;
-    refreshToken?: string;
-  }
+    interface Session extends DefaultSession {
+		accessToken?: string;
+		refreshToken?: string;
+    }
 
-  interface User {
-    accessToken?: string;
-    refreshToken?: string;
-  }
+    interface User {
+        accessToken?: string;
+        refreshToken?: string;
+    }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT {
-    accessToken?: string;
-    refreshToken?: string;
-  }
+    interface JWT {
+        accessToken?: string;
+        refreshToken?: string;
+    }
 }
