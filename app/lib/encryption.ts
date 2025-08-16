fe85e8b91c58e8565b3ab6ad3b3304b0e6763c8d
Alyoshka <alyoshkamyshko@gmail.com>
2025-08-15 19:18:23 -0700

    fix: deployment error caused by top level await

--- app/lib/encryption.ts
+++ app/lib/encryption.ts
@@ -1,21 +1,30 @@
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-const keyMaterial = await crypto.subtle.importKey(
-    "raw",
-    encoder.encode(process.env.ENCRYPTION_KEY!.slice(0, 32)),
-    { name: "AES-CBC" },
-    false,
-    ["encrypt", "decrypt"]
-);
+let keyMaterial: CryptoKey | null = null;
+
+async function getKeyMaterial(){
+	if (!keyMaterial) {
+		keyMaterial = await crypto.subtle.importKey(
+			"raw",
+			encoder.encode(process.env.ENCRYPTION_KEY!.slice(0, 32)),
+			{ name: "AES-CBC" },
+			false,
+			["encrypt", "decrypt"]
+		);
+	}
+
+	return keyMaterial;
+}
 
 export async function encrypt(text: string) {
+	let key = await getKeyMaterial();
     const iv = crypto.getRandomValues(new Uint8Array(16));
     const encoded = encoder.encode(text);
 
     const ciphertext = await crypto.subtle.encrypt(
         { name: "AES-CBC", iv },
-        keyMaterial,
+        key,
         encoded
     );
 
@@ -26,12 +35,13 @@ export async function encrypt(text: string) {
 }
 
 export async function decrypt(base64Data: string, base64Iv: string) {
+	let key = await getKeyMaterial();
     const iv = Uint8Array.from(Buffer.from(base64Iv, "base64"));
     const data = Uint8Array.from(Buffer.from(base64Data, "base64"));
 
     const decrypted = await crypto.subtle.decrypt(
         { name: "AES-CBC", iv },
-        keyMaterial,
+        key,
         data
     );
 
