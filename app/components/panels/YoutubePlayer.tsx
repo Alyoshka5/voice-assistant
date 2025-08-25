03522e143de31c2299d5e99e7d3649bf70c9851a
Alyoshka <alyoshkamyshko@gmail.com>
2025-08-25 12:36:33 -0700

    feat: add styles to YouTube video panel

--- app/components/panels/YoutubePlayer.tsx
+++ app/components/panels/YoutubePlayer.tsx
@@ -1,11 +1,17 @@
+import styles from  './Youtube.module.css'
 import ReactPlayer from 'react-player';
 
 export default function YoutubePlayer({ videoId }: { videoId: string }) {
     return (
-        <ReactPlayer
-            src={`https://www.youtube.com/watch?v=${videoId}`}
-            style={{margin: '2rem 0'}}
-            controls
-        />
+        <div className={styles.player_container}>
+            <div className={styles.player_wrapper}>
+                <ReactPlayer
+                    src={`https://www.youtube.com/watch?v=${videoId}`}
+                    width="100%"
+                    height="100%"
+                    controls
+                />
+            </div>
+        </div>
     );
 }
