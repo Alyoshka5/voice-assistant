d0e8fc5a098bbdfb6a22a7d84f951e0f973098ed
Alyoshka <alyoshkamyshko@gmail.com>
2025-08-29 13:22:28 -0700

    feat: add calendar controller to controller exporter

--- app/actions/categoryFunctions/controllerExporter.ts
+++ app/actions/categoryFunctions/controllerExporter.ts
@@ -3,11 +3,13 @@ import weatherFunctionController from "./weather/weatherController";
 import { CategoryControllers } from "@/app/types/types";
 import youtubeFunctionController from "./youtube/youtubeController";
 import tasksFunctionController from "./tasks/tasksController";
+import calendarFunctionController from "./calendar/calendarController";
 
 const categoryControllers: CategoryControllers = { 
     'weather': weatherFunctionController,
     'youtube': youtubeFunctionController,
     'tasks/todo': tasksFunctionController,
+    'calendar': calendarFunctionController,
     'other': getDefaultResponse,
  };
 
