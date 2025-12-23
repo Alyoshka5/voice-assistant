cd2c7796262075a6d65c5bf29d462e1b4786bbe3
Alyoshka <alyoshkamyshko@gmail.com>
2025-12-22 21:16:17 -0800

    fix: handle auth and API call errors

--- app/actions/categoryFunctions/weather/getCurrentWeather.ts
+++ app/actions/categoryFunctions/weather/getCurrentWeather.ts
@@ -1,15 +1,27 @@
 import { Conversation, Coordinates } from "@/app/types/types";
 import openAIClient from "@/app/lib/openai";
+import { auth } from "@/auth";
 
 const systemMessage = `
 You are a friendly voice assistant. Read the weather data and user request, and respond naturally, clearly, and briefly.
 Weather Data: `;
 
 export default async function getCurrentWeather(coordinates: Coordinates, conversation: Conversation) {
+    const session = await auth();
+    if (!session)
+        return {outputText: 'You need to be signed in to check your calendar events.'}
+
+    const accessToken = session.accessToken;
+    if (!accessToken)
+        return {outputText: 'You need to be signed in to check your calendar events.'}
+    
     const apiKey = process.env.GOOGLE_WEATHER_API_KEY;
     const url = `https://weather.googleapis.com/v1/currentConditions:lookup?key=${apiKey}&location.latitude=${coordinates.latitude}&location.longitude=${coordinates.longitude}`;
   
     const weatheRresponse = await fetch(url);
+    if (!weatheRresponse.ok)
+        return {outputText: `Sorry, I couldn't get the current weather.`}
+
     const weatherData = await weatheRresponse.json();
 
     weatherData.temperature.degrees = Math.round(weatherData.temperature.degrees);
@@ -18,13 +30,20 @@ export default async function getCurrentWeather(coordinates: Coordinates, conver
     weatherData.heatIndex.degrees = Math.round(weatherData.heatIndex.degrees);
     weatherData.windChill.degrees = Math.round(weatherData.windChill.degrees);
     
-    const openaiResponse = await openAIClient.responses.create({
-        model: "gpt-4.1-nano",
-        input: [
-            {role: 'system', content: systemMessage + JSON.stringify(weatherData)},
-            ...conversation,
-        ],
-    });
+    let openaiResponse 
+    try {
+        openaiResponse = await openAIClient.responses.create({
+            model: "gpt-4.1-nano",
+            input: [
+                {role: 'system', content: systemMessage + JSON.stringify(weatherData)},
+                ...conversation,
+            ],
+        });
+        if (openaiResponse.error)
+            throw new Error(openaiResponse.error.message);
+    } catch (error) {
+        return {outputText: `Sorry, I couldn't get the current weather.`}
+    }
 
     return {
         outputText: openaiResponse.output_text,
