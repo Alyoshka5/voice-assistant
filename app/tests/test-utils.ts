86d4b7b4ffb2f5b313ac9634b40bcf0b3b8179e3
Alyoshka <alyoshkamyshko@gmail.com>
2025-12-28 13:03:36 -0800

    build(tests): add db test utils

---
+++ app/tests/test-utils.ts
@@ -0,0 +1,24 @@
+import prisma from "@/app/lib/db";
+
+export async function clearDatabase() {
+    const tablenames = await prisma.$queryRawUnsafe<{ tablename: string }[]>(
+        `SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname='public'`
+    );
+
+    for (const { tablename } of tablenames) {
+        if (tablename !== '_prisma_migrations') {
+            await prisma.$executeRawUnsafe(`TRUNCATE TABLE "public"."${tablename}" CASCADE;`);
+        }
+    }
+}
+
+export async function resetDatabaseWithUser() {
+    clearDatabase();
+    
+    await prisma.user.create({
+        data: {
+            name: 'Test User Name',
+            email: 'test@example.com',
+        }
+    });
+}
